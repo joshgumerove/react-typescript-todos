3866688070c4a9a8cb63c72c64096e303b1dbed2
Josh Gumerove <jgumerove@kaufmanrossin.com>
2022-12-19 22:01:55 -0500

    statically render todos

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from "react";
+import Todos from "./components/Todos";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello world</h1>
+      <Todos />
     </div>
   );
 }
